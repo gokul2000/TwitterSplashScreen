a1378d0485be99d38914cf0f45e437e2551bb6b3
gokul2000 <ngokulnarayanan@gmail.com>
2020-07-15 16:23:01 +0530

    Initial commit

---
+++ app/src/main/java/com/example/twittersplashscreen/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.twittersplashscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(intent);
+
+            }
+        },2500);
+
+    }
+}
